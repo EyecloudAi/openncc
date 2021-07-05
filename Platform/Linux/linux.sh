322fab7b886db7eb6566075d6a3ba2ffdb05f58a
beam <liangjie.song@eyecloud.tech>
2021-07-05 16:31:46 +0800

    chmod +x linux.sh

--- Platform/Linux/linux.sh
+++ Platform/Linux/linux.sh
@@ -1,71 +1,141 @@
 #!/bin/bash
 
+
+
 project_path=$(cd `dirname $0`;pwd)
+
 project_name="${project_path##*/}"
+
 #echo $project_path
+
 echo $project_name
 
+
+
 echo -n "Please make sure you have backed up what you need?(y or n)"
+
 read Key
+
 if [ $Key = "y" ];then
+
 	rm -rf Source/ Example/ Viewer/
+
 elif [ $Key = "n" ];then
+
 	echo "n"
+
 else
+
 	echo "other"
+
 fi
+
 echo "mkdir Source"
+
 if [ ! -d "Source/" ];then
+
 	mkdir Source
+
 else
+
 	echo "Source is exist"
+
 fi
+
+
 
 echo "copy Library ...."
+
 if [ ! -d "Source/Library/" ];then
+
 	cp -r ../../SDK/Source/Library/Linux/ ./Source/Library
+
 else
+
 	echo "Library is exist"
+
 fi
+
+
 
 echo "copy Model ..... "
+
 if [ ! -d "Source/Model/" ];then
+
 	cp -r ../../SDK/Source/Model/ ./Source
+
 else
+
 	echo "Model is exist"
+
 fi
+
+
 
 echo "copy Firmware ..... "
+
 if [ ! -d "Source/Firmware/" ];then
+
 	cp -r ../../SDK/Source/Firmware/ ./Source
+
 else
+
 	echo "Firmware is exist"
+
 	if [ ! -f "Source/Firmware/OpenNcc.mvcmd" ];then
+
 		cp ../../SDK/Source/Firmware/OpenNcc.mvcmd Source/Firmware/
+
 	else
+
 		echo "OpenNcc.mvcmd is exist,don't need copy"
+
 	fi
+
 fi
+
+
 
 echo "copy Example ..... "
+
 if [ ! -d "Example/" ];then
+
 	cp -r ../../SDK/Example/ ./
+
 else
+
 	echo "Example is exist"
-fi
-rm -rf Example/How_to/Python_demo
-echo "copy Viewer ...... "
-if [ ! -d "Viewer/" ];then
-	cp -r ../../SDK/Viewer/ ./
-	chmod +x Viewer/OpenNcc_Linux/OpenNCC
-	rm -rf Viewer/OpenNcc_Linux/AppRun
-	ln Viewer/OpenNcc_Linux/OpenNCC Viewer/OpenNcc_Linux/AppRun
-	chmod +x Viewer/OpenNcc_Linux/Configuration/moviUsbBoot
-	rm -rf Viewer/OpenNcc_Windows
-	rm -rf Viewer/OpenNCC_Raspberry
-else
-	echo "Viewer is exist"
+
 fi
 
+rm -rf Example/How_to/Python_demo
+
+echo "copy Viewer ...... "
+
+if [ ! -d "Viewer/" ];then
+
+	cp -r ../../SDK/Viewer/ ./
+
+	chmod +x Viewer/OpenNcc_Linux/OpenNCC
+
+	rm -rf Viewer/OpenNcc_Linux/AppRun
+
+	ln Viewer/OpenNcc_Linux/OpenNCC Viewer/OpenNcc_Linux/AppRun
+
+	chmod +x Viewer/OpenNcc_Linux/Configuration/moviUsbBoot
+
+	rm -rf Viewer/OpenNcc_Windows
+
+	rm -rf Viewer/OpenNCC_Raspberry
+
+else
+
+	echo "Viewer is exist"
+
+fi
+
+
+
 echo "copy Linux lib to Viewer"
+
 cp Source/Library/C\&C++/libOpenNCC.a Viewer/QT_Package/OpenNCC/ncc_sdk/libOpenNCC.a
